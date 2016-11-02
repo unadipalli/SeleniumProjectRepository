71cb04c92d69e261236fde1c79af736c22807555
umanadipalli <umanadipalli@UMA>
2016-11-02 17:16:59 -0400

    Moving files to Github

---
+++ Selenium_Project/src/openApp.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class openApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver chdriver = new ChromeDriver();
+		chdriver.get("https://www.facebook.com/");
+		System.out.println("Face book is open");
+	}
+
+}
